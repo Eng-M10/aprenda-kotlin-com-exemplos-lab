b117b6819bec25d21be3e875e0bf81c09056f8c1
Muvimbene Daniel Maposse <87478383+Eng-M10@users.noreply.github.com>
2023-10-16 13:41:59 +0200

    feat: Adicionando a minha solucao ao desafio de codigo, as funcionalidade de inscricao

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,60 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIO, AVANCADO }
 
-class Usuario
+data class Usuario (val nome: String, var email: String, var planoDio: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int, val nivel:Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        	
+         inscritos.add(usuario)
+        
+        
     }
+    
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Crie alguns objetos para simular cenários de teste
+    val usuario1 = Usuario("Alex Garcia" , "A.garcia@outlook.com", "Dio Global")
+    val usuario2 = Usuario("Silvia Andrade", "03garotinha80@gmail.com", "Dio Global")
+
+    val conteudo1 = ConteudoEducacional("Curso de Kotlin", 120, Nivel.INTERMEDIO)
+    val conteudo2 = ConteudoEducacional("Curso de Android", 180,Nivel.AVANCADO)
+    val conteudo3 = ConteudoEducacional("Curso de Java",100,Nivel.INTERMEDIO)
+    val conteudo4 = ConteudoEducacional("Curso de Introducao ao Desenvolvimento Web", 160, Nivel.BASICO)
+
+    val formacao1 = Formacao("Desenvolvimento Android", mutableListOf(conteudo1, conteudo2))
+    val formacao2 = Formacao("Desenvolvimento Backend com Kotlin e Java",mutableListOf(conteudo4,conteudo1,conteudo3)) 
+
+    // Matricular usuários na formação
+    formacao1.matricular(usuario1)
+    formacao2.matricular(usuario2)
+
+    // Verificar a lista de inscritos
+    println("Inscritos na formação '${formacao1.nome}':")
+    println("Conteudos :")
+    for(conteudo in formacao1.conteudos)
+    println("${conteudo.nome.toString()}  ${conteudo.duracao.toString()}H  ${conteudo.nivel.toString().lowercase().capitalize()} ")
+    for (usuario in formacao1.inscritos) {
+        println("Nome --------------Plano")
+        println(usuario.nome.toString() +"---"+ usuario.planoDio.toString())
+      
+     }
+            println("\n \n ")
+      println("Inscritos na formação '${formacao2.nome}': ")
+      for(conteudo in formacao2.conteudos)
+    println("${conteudo.nome.toString()}  ${conteudo.duracao.toString()}H  ${conteudo.nivel.toString().lowercase().capitalize()} ")
+    for (usuario in formacao2.inscritos) {
+        println("Nome --------------Plano")
+        println(usuario.nome.toString() +"---"+ usuario.planoDio.toString())
+      
+     }
 }
